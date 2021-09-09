18b6f0551d8e6b4fc42d0a5ddea16efd8c86d026
J.D. Zamfirescu <zamfi@alum.mit.edu>
2021-09-09 14:01:24 -0700

    unregistering service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Rudy from './rudy';
 // import registerServiceWorker from './registerServiceWorker';
+import unregister from './registerServiceWorker';
 
 ReactDOM.render(<Rudy />, document.getElementById('root'));
 // registerServiceWorker();
+unregister();
